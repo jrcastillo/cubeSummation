611c9b4ed643c597e08122fbe4fc3532b0e17f2f
Ramon Castillo <jrcastillo200@gmail.com>
2018-01-22 16:47:51 -0500

    adding a flow control variable

--- src/view/main.js
+++ src/view/main.js
@@ -1,7 +1,10 @@
-"use strict";
+'use strict';
 
 const readline  = require('readline');
 const interfaceController = require('../controller/interface-controller.js');
+
+let initOperationsCounter = 2;
+
 let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -9,29 +12,35 @@ let rl = readline.createInterface({
 
 console.log('******* Cube Summation Challenge *********');
 console.log('******* Input number of test cases *********');
-
+console.log('Input amount of test cases');
 
 rl.on('line', (line) => {
-  if(line == 'exit')
+  if(line === 'exit')
     rl.close();
 
   try{
     let command = line.split(' ');
-    if(command.length == 1) {
-      console.log(interfaceController.setOperationsCounter(command));
-      rl.resume();
-    } else if (command.length == 2) {
-      console.log(interfaceController.setTransactionsCounter(command));
-      rl.resume();
-    } else if (command.length == 5 || command.length == 7) {
-      console.log(interfaceController.excecuteTransaction(command));
-      rl.resume();
+    if(initOperationsCounter === 2 && command.length === 1 && command[0] !== '') {
+      initOperationsCounter--;
+      interfaceController.setOperationsCounter(command);
+    } else if (initOperationsCounter === 1 && command.length === 2) {
+      if(interfaceController.setTransactionsCounter(command)){
+        initOperationsCounter--;
+      }
+    } else if (initOperationsCounter === 0 && command.length === 5 || command.length === 7) {
+      interfaceController.executeTransaction(command);
+      if(interfaceController.checkTestCases()){
+          initOperationsCounter = 1;
+      }
     } else {
+      initOperationsCounter = 2;
       interfaceController.resetApp();
-      rl.write('error writing commands, restarting');
-      return '';
+      console.log('error writing commands, please start over');
+      console.log('');
+      console.log('Input amount of test cases');
     }
 
+    rl.resume();
   } catch(err){
     console.log(err.toString());
     interfaceController.resetApp();
